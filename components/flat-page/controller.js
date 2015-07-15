7334a754de0a326199e5426ab7d14ac422acee25
Владимир Тицкий <vladimir.titskiy@inessoft.kz>
2015-07-15 20:26:26 +0600

    new: ad page

--- components/flat-page/controller.js
+++ components/flat-page/controller.js
@@ -6,6 +6,10 @@ angular.module('getflat.flat-page',[])
     var flat = this,
         flat_id = $routeParams.id;
 
+    flat.rtype = { H: "час", D: "день",  M: "месяц" };
+
+    flat.showContacts = false;
+
     flatPageRest.getFlat(flat_id,
       function(status, response) {
         if(status === 'success') flat.info = response;
