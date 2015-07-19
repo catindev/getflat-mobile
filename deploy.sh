ff5ad222df489f0bb58ba40631584fb622dd9616
Владимир Тицкий <vladimir.titskiy@inessoft.kz>
2015-07-20 01:34:07 +0600

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-pm2 stop m-getflat && git pull && npm i && bower i --allow-root && gulp index && pm2 start m-getflat.js
+git pull && npm i && bower i --allow-root && gulp index && pm2 restart m-getflat
